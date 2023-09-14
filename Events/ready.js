4419921a89698087a1c9882ae9586247fc47c378
Cirec-Coder <cirec.coder@gmail.com>
2023-09-14 07:50:25 +0200

    update readyEvent

--- Events/ready.js
+++ Events/ready.js
@@ -3,5 +3,5 @@ const Discord = require('discord.js');
 module.exports = async bot => {
     bot.user.setAvatar('./src/assets/img/avatar.png').catch(console.error);
     bot.user.setActivity("The Warriors's Bot");
-    console.log(`${bot.user.tag} est bien en ligne !`);
+    console.log(`${bot.user.username} est bien en ligne !`);
 }
