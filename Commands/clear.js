79d6d0c062bb9d2985a6308d2ca876ff9b1bb9f3
Cirec-Coder <cirec.coder@gmail.com>
2023-09-18 14:09:41 +0200

    change nm max messages to delete

--- Commands/clear.js
+++ Commands/clear.js
@@ -2,7 +2,7 @@ const Discord = require("discord.js");
 
 module.exports = {
     name: "clear",
-    description: "Clear les messages du salon (par défaut 100)",
+    description: "Clear les messages du salon (par défaut 1)",
     permission: Discord.PermissionFlagsBits.ManageMessages,
     dm: false,
     category: "Moderation",
@@ -23,7 +23,7 @@ module.exports = {
 		const messages = await message.channel.messages.fetch({limit: nbMsg});
 		const messagesToDelete = messages.filter((m) => Date.now() - m.createdTimestamp < 1209600 * 1000);
 
-		if (!nbMsg) nbMsg = messagesToDelete.size;
+		if (!nbMsg) nbMsg = 1; //messagesToDelete.size;
 
 		if (nbMsg > 100) return message.reply("Le nombre de message ne peut pas être supérieur à 100 !");
 
