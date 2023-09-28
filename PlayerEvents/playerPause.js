f35bb2565cca9f4c1af9fd8f802ef1f323cb4401
Cirec-Coder <cirec.coder@gmail.com>
2023-09-28 13:05:41 +0200

    Add bot Activity

--- PlayerEvents/playerPause.js
+++ PlayerEvents/playerPause.js
@@ -1,4 +1,7 @@
-module.exports = async (bot, queue, track) =>{
+const Discord = require("discord.js");
+module.exports = async (bot, queue, args) =>{
 
+    const track = queue.currentTrack;
+    bot.user.setActivity("En pause : " + track.title, {type: Discord.ActivityType.Custom});
     queue.channel.send(`Le player est en pause !`)
 }
--- PlayerEvents/playerResume.js
+++ PlayerEvents/playerResume.js
@@ -1,6 +1,8 @@
+const Discord = require("discord.js");
 module.exports = async (bot, interaction, args) =>{
 
     const queue = await bot.player.nodes.get(interaction.guild);
     const track = queue.currentTrack;
+    bot.user.setActivity(track.title, {type: Discord.ActivityType["Listening"]});
     interaction.channel.send(`Le lecteur a repris la lecture de :\n${bot.myEmojis.musique} ${track}`)
 }
